9e79c92a896875c2608a70962d59d85deb2a96fd
Script <bigalex934@gmail.com>
2025-03-08 19:34:00 +0300

    Remove gtest dependency

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  void *data;
+  node *next;
+  node *prev;
+} node;
 
 typedef struct queue {
-
+  node *head;
+  size_t size;
 } queue;
 
-queue *create_queue() {}
+queue *create_queue() { queue *q = (queue *)malloc(sizeof(queue)); }
 queue *release_queue() {}
 
 void push_queue(queue *queue, void *data) {}
