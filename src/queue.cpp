bfc58443c16b189ea0ad76aa65221d0af9b5d7aa
Script <bigalex934@gmail.com>
2025-03-08 18:59:54 +0300

    Add queue related files

---
+++ src/queue.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct queue {
+
+} queue;
+
+queue *create_queue() {}
+queue *release_queue() {}
+
+void push_queue(queue *queue, void *data) {}
+void *queue_last_item() {}
+void pop_queue(queue *queue) {}
+size_t queue_size(queue *queue) {}
